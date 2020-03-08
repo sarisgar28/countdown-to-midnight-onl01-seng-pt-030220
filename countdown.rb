76aba0337d4a8e6b7429bdc466ff102af4b3cde2
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-08 20:10:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,37 @@
-#write your code here
+# input -> number of seconds -> integer 
+#output -> return "HAPPY NEW YEAR" = string 
 
-def countdown
-end
+#while seconds are greater than zero 
+# number of seconds 
+#decrease seconds by 1 
+#end 
+#return "happy new year"
+#End 
+
+def countdown(seconds)
+ while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds = seconds - 1 
+  end
+  return "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+   puts "#{seconds} SECOND(S)!"
+   seconds -= 1 
+   sleep(1) 
+  end
+  return "HAPPY NEW YEAR!"
+end   
+
+
+
+
+
+
+
+
+
+
+
