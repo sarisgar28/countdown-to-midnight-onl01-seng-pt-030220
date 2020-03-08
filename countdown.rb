bf97e05e9d44be0b68d0f60abcbbfb2f95a948ff
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-08 19:56:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,1 +1,11 @@
-# input -> number of seconds -> integer
+# input -> number of seconds -> integer 
+#output -> return "HAPPY NEW YEAR" = string 
+
+#while seconds are greater than zero 
+# number of seconds 
+#decrease seconds by 1 
+#end 
+#return "happy new year"
+#End 
+
+def countdown 
