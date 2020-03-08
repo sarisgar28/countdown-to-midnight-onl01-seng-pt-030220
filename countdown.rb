b39857d2c3bcac06225e0e6f570033d2b70a65fd
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-08 20:10:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -15,3 +15,23 @@ def countdown(seconds)
   end
   return "HAPPY NEW YEAR!"
 end 
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+   puts "#{seconds} SECOND(S)!"
+   seconds -= 1 
+   sleep(1) 
+  end
+  return "HAPPY NEW YEAR!"
+end   
+
+
+
+
+
+
+
+
+
+
+
